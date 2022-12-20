e8bac879a83fc29b67df8627e2172324417ca601
maha <210103118@stu.sdu.edu.kz>
2022-12-20 14:53:36 +0600

    #

---
+++ cart.js
@@ -0,0 +1,80 @@
+let carts = document.querySelectorAll('.add-cart');
+
+let products = [
+    {
+        name: 'Grey Tshirt',
+        tag: 'greytshirt',
+        price: 15,
+        inCart: 0
+    },
+    {
+        name: 'Grey Hoodie',
+        tag: 'greyhoodie',
+        price: 20,
+        inCart: 0
+    }
+];
+
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+        totalCost(products[i])
+    })
+}
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if (productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers(product) {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+    setItems(product);
+}
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if (cartItems != null) {
+        if (cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+    cartCost = parseInt(cartCost);
+    console.log("My cartCost is", cartCost);
+    console.log(typeof cartCost);
+    if (cartCost != null) {
+        localStorage.setItem("totalCost", cartCost + product.price);
+    } else {
+        localStorage.setItem("totalCost", product.price);
+    }
+}
+
+onLoadCartNumbers();
